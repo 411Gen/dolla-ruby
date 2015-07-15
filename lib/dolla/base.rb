896da2886da2b7ec3630dfacf4f9723652fd61f4
Tamiswanashe Chipika <tchipika@gmail.com>
2015-07-15 18:00:18 +0200

    For actual use we'll leave it pointing to lvh.me

--- lib/dolla/base.rb
+++ lib/dolla/base.rb
@@ -3,7 +3,8 @@ require "dolla/json_api_client/middleware/dolla_environment"
 
 module Dolla
   class Base < ::JsonApiClient::Resource
-    self.site = "https://api.dollapayments.com/"
+    #self.site = "https://api.dollapayments.com/"
+    self.site = "http://api.lvh.me:5000/"
 
     self.connection do |connection|
       connection.use JsonApiClient::Middleware::Authentication
