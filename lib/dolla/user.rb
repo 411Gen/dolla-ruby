f448418378a1161cc2d0ac6a03608c510b4ead90
Tamiswanashe Chipika <tamiswa@Tamiswanashes-MacBook-Pro.local>
2017-05-03 02:49:00 +0200

    Update

--- lib/dolla/user.rb
+++ lib/dolla/user.rb
@@ -12,9 +12,5 @@ module Dolla
     custom_endpoint :unlock, on: :member, request_method: :put
 
     # has_many :cards
-
-    class Transaction < Dolla::Base
-      belongs_to :user
-    end
   end
 end
