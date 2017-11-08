682268b5e1e9ec004eba737e71522db839c7595b
Tamiswanashe Chipika <tchipika@issecode.com>
2017-11-08 14:47:41 +0200

    Start work on library side validation

--- lib/dolla/merchant.rb
+++ lib/dolla/merchant.rb
@@ -2,6 +2,9 @@ require 'dolla/base'
 
 module Dolla
   class Merchant < Dolla::Base
+    property :name
+
+    validates :name, presence: true
 
     class Customer < Dolla::Base
       belongs_to :merchant#, class_name: '::Dolla::Merchant'
