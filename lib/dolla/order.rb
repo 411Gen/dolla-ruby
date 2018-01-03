f4bca4a07c13912324ac478805b79bfe99d0384b
Tamiswanashe Chipika <tchipika@issecode.com>
2018-01-03 12:10:35 +0200

    Forgot an endpoint

--- lib/dolla/order.rb
+++ lib/dolla/order.rb
@@ -6,6 +6,7 @@ module Dolla
     custom_endpoint :next, on: :member, request_method: :put
     custom_endpoint :complete, on: :member, request_method: :put
     custom_endpoint :acknowledge, on: :member, request_method: :put
+    custom_endpoint :ship, on: :member, request_method: :put
     custom_endpoint :receive, on: :member, request_method: :put
   end
 end
