6e598bcf6ab4b7dbf1feafc802cb33fddd944317
Tamiswanashe Chipika <tchipika@issecode.com>
2018-01-24 16:45:48 +0200

    Add endpoint for featured products

--- lib/dolla/product.rb
+++ lib/dolla/product.rb
@@ -4,5 +4,6 @@ module Dolla
   class Product < Dolla::Base
 
     custom_endpoint :reprocess_images, on: :member, request_method: :put
+    custom_endpoint :featured, on: :collection, request_method: :get
   end
 end
