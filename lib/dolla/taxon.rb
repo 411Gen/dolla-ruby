2f6f99217189a6a90ed56f61271b20d2c7ccb9c8
Tamiswanashe Chipika <tchipika@issecode.com>
2018-02-06 13:00:34 +0200

    More love to taxon resource

--- lib/dolla/taxon.rb
+++ lib/dolla/taxon.rb
@@ -2,6 +2,11 @@ require 'dolla/base'
 
 module Dolla
   class Taxon < Dolla::Base
+    property :name
+
+    validates :name, presence: true
+
+    has_many :products
 
     custom_endpoint :add_product, on: :member, request_method: :post
     custom_endpoint :remove_product, on: :member, request_method: :delete
