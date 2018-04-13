2acc99f78f2aa657f8c59c816f8eca8bf4dea573
Tamiswanashe Chipika <tchipika@issecode.com>
2018-04-13 14:09:24 +0200

    Added more endpoints

--- lib/dolla/product.rb
+++ lib/dolla/product.rb
@@ -6,6 +6,8 @@ module Dolla
     custom_endpoint :reprocess_images, on: :member, request_method: :put
     custom_endpoint :featured, on: :collection, request_method: :get
     custom_endpoint :search_marketplace, on: :collection, request_method: :get
+    custom_endpoint :search_marketplace_by_image, on: :collection, request_method: :get
+    custom_endpoint :find_by_image, on: :collection, request_method: :get
 
     custom_endpoint :like, on: :member, request_method: :post
     custom_endpoint :dislike, on: :member, request_method: :post
