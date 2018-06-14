7c1037837334710a79eccbf0a50724d39d62d173
Tamiswanashe Chipika <tchipika@issecode.com>
2018-06-14 15:41:51 +0200

    Update lib

--- lib/dolla/user.rb
+++ lib/dolla/user.rb
@@ -5,16 +5,22 @@ module Dolla
 
     # has_many :cards, class_name: "Dolla::Card"
 
-    custom_endpoint :confirmation, on: :member, request_method: :put
+    custom_endpoint :confirmations, on: :member, request_method: :put
 
-    custom_endpoint :password, on: :member, request_method: :put
+    custom_endpoint :passwords, on: :member, request_method: :put
 
-    custom_endpoint :unlock, on: :member, request_method: :put
+    custom_endpoint :unlocks, on: :member, request_method: :put
 
     class Order < Dolla::Base
       belongs_to :user
     end
 
-    # has_many :cards
+    class Confirmation < Dolla::Base
+      belongs_to :user
+    end
+
+    class Password < Dolla::Base
+      belongs_to :user
+    end
   end
 end
