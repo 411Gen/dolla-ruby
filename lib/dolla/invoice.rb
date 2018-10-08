992f0b6449273222aac96f5b6ebaf4cecd1ca3a0
Tamiswanashe Chipika <tchipika@issecode.com>
2018-10-08 15:37:56 +0200

    One more endpoint

--- lib/dolla/invoice.rb
+++ lib/dolla/invoice.rb
@@ -4,5 +4,7 @@ module Dolla
   class Invoice < Dolla::Base
     has_one :merchant, class_name: "Dolla::Merchant"
     has_one :customer, class_name: "Dolla::Customer"
+
+    custom_endpoint :mark_paid, on: :member, request_method: :put
   end
 end
