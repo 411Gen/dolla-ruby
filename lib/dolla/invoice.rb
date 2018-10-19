c2b6eacff3f4567f30651613e996964712a04fdc
Tamiswanashe Chipika <tchipika@issecode.com>
2018-10-19 22:59:18 +0200

    Old invoice features don't matter anymore

--- lib/dolla/invoice.rb
+++ lib/dolla/invoice.rb
@@ -2,8 +2,8 @@ require 'dolla/base'
 
 module Dolla
   class Invoice < Dolla::Base
-    has_one :merchant, class_name: "Dolla::Merchant"
-    has_one :customer, class_name: "Dolla::Customer"
+    # has_one :merchant, class_name: "Dolla::Merchant"
+    # has_one :customer, class_name: "Dolla::Customer"
 
     custom_endpoint :mark_paid, on: :member, request_method: :put
   end
