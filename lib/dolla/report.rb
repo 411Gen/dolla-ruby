e6d1ee6cefe9edcbb0ebc24588d5c045a281bbf0
Tamiswanashe Chipika <tchipika@issecode.com>
2019-01-09 10:33:48 +0200

    Add endpoint

--- lib/dolla/report.rb
+++ lib/dolla/report.rb
@@ -5,6 +5,7 @@ module Dolla
 
     custom_endpoint :dashboard, on: :collection, request_method: :get
     custom_endpoint :total_revenue, on: :collection, request_method: :get
+    custom_endpoint :products_series, on: :collection, request_method: :get
     custom_endpoint :merchants_series, on: :collection, request_method: :get
     custom_endpoint :customers_series, on: :collection, request_method: :get
     custom_endpoint :transactions_series, on: :collection, request_method: :get
