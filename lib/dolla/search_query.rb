81b8ea0cd3c37ea03399ee931610c53c728c14be
Tamiswanashe Chipika <tchipika@issecode.com>
2019-12-12 19:49:55 +0200

    Fix bug

--- lib/dolla/search_query.rb
+++ lib/dolla/search_query.rb
@@ -3,5 +3,6 @@ require 'dolla/base'
 module Dolla
   class SearchQuery < Dolla::Base
     custom_endpoint :convert, on: :member, request_method: :put
+    custom_endpoint :completions, on: :collection, request_method: :get
   end
 end
